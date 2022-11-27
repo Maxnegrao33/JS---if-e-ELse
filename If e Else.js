c0dc5ea3dcabf48732073cae44686acd6fd69799
Maciel Ribeiro de Oliveira <38005099+Maxnegrao33@users.noreply.github.com>
2022-11-27 16:54:42 -0300

    Add files via upload

---
+++ If e Else.js
@@ -0,0 +1,38 @@
+/*If e else são estruturas de decisão (condicionais)
+**Onde há a possibilidade de escolha entre duas ou mais
+**condições apresentadas no código
+*/
+
+let idade = 90;
+if (idade >= 18) {
+    console.log("Você é MAIOR de idade");
+} else {
+    console.log("Você é MENOR de idade");
+}
+
+//Para saber se a idade é igual, usa-se dois sinais de igual (==)
+let idade2 = 89;
+if (idade2 == 18) {
+    console.log("Você é MAIOR de idade");
+} else {
+    console.log("Você é MENOR de idade");
+}
+
+//Apenas um sinal de igual remonta à atribuição de valores
+/*Os símbolos de atribuição que podem ser usados são esses:
+< - Maior
+< - Menor
+== - Igual
+<= - Menor ou igual
+>= - Maior ou igual
+
+*/
+
+
+
+
+
+
+
+
+
